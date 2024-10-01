3c42d919d5882a4d06688da08bc44eb765d3c750
Mateusz Wittek <mati128xd@gmail.com>
2024-10-01 14:12:11 +0200

    person with id endpoint added

--- src/server.js
+++ src/server.js
@@ -24,6 +24,21 @@ app.get('/people', async (req, res) => {
   }
 });
 
+app.get('/people/:id', async (req, res) => {
+  try {
+    const filePath = path.join(__dirname, '/people/people-data.json');
+    const peopleData = await getFileData(filePath);
+    const person = peopleData.find(person => person.id === Number(req.params.id));
+    if (!person) {
+      res.status(404).send('Person not found');
+    } else {
+      res.json(person);
+    }
+  } catch (error) {
+    res.status(500).send(`Error reading file: ${error}`);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
