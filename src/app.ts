988ae5b9d8d984cc712a7ba1125b2eb31622f3af
Mateusz Wittek <mati128xd@gmail.com>
2024-10-09 12:52:46 +0200

    add error handling for bad routes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express, { Application } from 'express';
+import express from 'express';
 import cors from 'cors';
 import connectDB from './config/database.js';
 import peopleRouter from './routes/peopleRoutes.js';
-import { errorHandler } from './utils/errorHandler.js';
+import { errors, errorHandler } from './utils/errorHandler.js';
 
-const app: Application = express();
+const app: express.Application = express();
 
 connectDB();
 
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/people', peopleRouter);
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  next(errors.NOT_FOUND());
+});
+
 app.use(errorHandler);
 
 export default app;
